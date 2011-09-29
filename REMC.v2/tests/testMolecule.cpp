ba9f454c81f8903b39e6c7b0dd96c601cecf7bb8
Adrianna Pińska <adrianna.pinska@gmail.com>
2011-09-29 16:34:25 +0200

    beginnings of a molecule test

--- REMC.v2/tests/testMolecule.cpp
+++ REMC.v2/tests/testMolecule.cpp
@@ -1,6 +1,24 @@
 #include <UnitTest++.h>
 #include <Molecule.h>
 
-// TEST(TestMolecule) {
-//     CHECK(false);
-// }
+struct MoleculeFixture
+{
+    Molecule molecule;
+//     char* pdbfilename;
+    AminoAcids aminoAcidData;
+
+    MoleculeFixture()
+    {
+        molecule.AminoAcidsData = aminoAcidData;
+        molecule.index = 0;
+        molecule.initFromPDB("data/conf1/1a.pdb");
+    }
+
+    ~MoleculeFixture()
+    {
+    }
+};
+
+TEST_FIXTURE(MoleculeFixture, TestMolecule) {
+    CHECK_EQUAL(24, molecule.residueCount);
+}
