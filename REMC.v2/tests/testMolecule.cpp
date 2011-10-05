ba34cc4b42739cb112ed7d5ffd9e762159d1c25b
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2011-10-05 10:15:23 +0200

    reading from pdb test

--- REMC.v2/tests/testMolecule.cpp
+++ REMC.v2/tests/testMolecule.cpp
@@ -1,10 +1,10 @@
 #include <UnitTest++.h>
+#include <ctype.h>
 #include <Molecule.h>
 
 struct MoleculeFixture
 {
     Molecule molecule;
-//     char* pdbfilename;
     AminoAcids aminoAcidData;
 
     MoleculeFixture()
@@ -22,11 +22,19 @@ struct MoleculeFixture
     }
 };
 
-TEST_FIXTURE(MoleculeFixture, TestMolecule) {
-    CHECK_EQUAL(76, molecule.residueCount);
-    cout << aminoAcidData.data[molecule.Residues[0].aminoAcidIndex].sname << endl;
-    cout << aminoAcidData.data[molecule.Residues[1].aminoAcidIndex].sname << endl;
-    cout << aminoAcidData.data[molecule.Residues[2].aminoAcidIndex].sname << endl;
+TEST_FIXTURE(MoleculeFixture, TestInitFromPDB) {
+    const int expected_size = 76;
+    CHECK_EQUAL(expected_size, molecule.residueCount);
 
-    char expected_sequence[76][4] = {"MET", "GLN", "ILE", "PHE", "VAL", "LYS", "THR", "LEU", "THR", "GLY", "LYS", "THR", "ILE", "THR", "LEU", "GLU", "VAL", "GLU", "PRO", "SER", "ASP", "THR", "ILE", "GLU", "ASN", "VAL", "LYS", "ALA", "LYS", "ILE", "GLN", "ASP", "LYS", "GLU", "GLY", "ILE", "PRO", "PRO", "ASP", "GLN", "GLN", "ARG", "LEU", "ILE", "PHE", "ALA", "GLY", "LYS", "GLN", "LEU", "GLU", "ASP", "GLY", "ARG", "THR", "LEU", "SER", "ASP", "TYR", "ASN", "ILE", "GLN", "LYS", "GLU", "SER", "THR", "LEU", "HIS", "LEU", "VAL", "LEU", "ARG", "LEU", "ARG", "GLY", "GLY"}
+    char expected_sequence_names[expected_size][4] = {"MET", "GLN", "ILE", "PHE", "VAL", "LYS", "THR", "LEU", "THR", "GLY", "LYS", "THR", "ILE", "THR", "LEU", "GLU", "VAL", "GLU", "PRO", "SER", "ASP", "THR", "ILE", "GLU", "ASN", "VAL", "LYS", "ALA", "LYS", "ILE", "GLN", "ASP", "LYS", "GLU", "GLY", "ILE", "PRO", "PRO", "ASP", "GLN", "GLN", "ARG", "LEU", "ILE", "PHE", "ALA", "GLY", "LYS", "GLN", "LEU", "GLU", "ASP", "GLY", "ARG", "THR", "LEU", "SER", "ASP", "TYR", "ASN", "ILE", "GLN", "LYS", "GLU", "SER", "THR", "LEU", "HIS", "LEU", "VAL", "LEU", "ARG", "LEU", "ARG", "GLY", "GLY"};
+    int expected_sequence[expected_size];
+    int got_sequence[expected_size];
+
+    for (int i = 0; i < expected_size; i++)
+    {
+        expected_sequence[i] = aminoAcidData.getAminoAcidIndex(expected_sequence_names[i]);
+        got_sequence[i] = molecule.Residues[i].aminoAcidIndex;
+    }
+
+    CHECK_ARRAY_EQUAL(expected_sequence, got_sequence, expected_size);
 }
