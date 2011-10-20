97e2c34de52753a862f7c1e44f4b222bc68a0640
Adrianna Pińska <adrianna.pinska@gmail.com>
2011-10-20 14:57:21 +0200

    switched to cppunit

--- REMC.v2/tests/testMolecule.cpp
+++ REMC.v2/tests/testMolecule.cpp
@@ -1,45 +1,60 @@
-#include <UnitTest++.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
 #include <ctype.h>
 #include <Molecule.h>
 
-struct MoleculeFixture
+class TestMolecule : public CppUnit::TestFixture
 {
+    CPPUNIT_TEST_SUITE(TestMolecule);
+    CPPUNIT_TEST(testPDBSequence);
+    CPPUNIT_TEST(testPDBCentre);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
     Molecule molecule;
     AminoAcids aminoAcidData;
 
-    MoleculeFixture()
-    {
-        aminoAcidData.loadAminoAcidData(AMINOACIDDATASOURCE);
-        aminoAcidData.loadLJPotentialData(LJPDSOURCE);
-
-        molecule.AminoAcidsData = aminoAcidData;
-        molecule.index = 0;
-        molecule.initFromPDB("tests/1UBQ.pdb");
-    }
-
-    ~MoleculeFixture()
-    {
-    }
+public:
+    void setUp();
+    void testPDBSequence();
+    void testPDBCentre();
 };
 
-TEST_FIXTURE(MoleculeFixture, TestInitFromPDB) {
+CPPUNIT_TEST_SUITE_REGISTRATION(TestMolecule);
+
+void TestMolecule::setUp()
+{
+    aminoAcidData.loadAminoAcidData(AMINOACIDDATASOURCE);
+    aminoAcidData.loadLJPotentialData(LJPDSOURCE);
+
+    molecule.AminoAcidsData = aminoAcidData;
+    molecule.index = 0;
+    molecule.initFromPDB("tests/1UBQ.pdb");
+}
+
+void TestMolecule::testPDBSequence()
+{
     const int expected_size = 76;
-    CHECK_EQUAL(expected_size, molecule.residueCount);
+    CPPUNIT_ASSERT_EQUAL(expected_size, molecule.residueCount);
 
     char expected_sequence_names[expected_size][4] = {"MET", "GLN", "ILE", "PHE", "VAL", "LYS", "THR", "LEU", "THR", "GLY", "LYS", "THR", "ILE", "THR", "LEU", "GLU", "VAL", "GLU", "PRO", "SER", "ASP", "THR", "ILE", "GLU", "ASN", "VAL", "LYS", "ALA", "LYS", "ILE", "GLN", "ASP", "LYS", "GLU", "GLY", "ILE", "PRO", "PRO", "ASP", "GLN", "GLN", "ARG", "LEU", "ILE", "PHE", "ALA", "GLY", "LYS", "GLN", "LEU", "GLU", "ASP", "GLY", "ARG", "THR", "LEU", "SER", "ASP", "TYR", "ASN", "ILE", "GLN", "LYS", "GLU", "SER", "THR", "LEU", "HIS", "LEU", "VAL", "LEU", "ARG", "LEU", "ARG", "GLY", "GLY"};
-    int expected_sequence[expected_size];
-    int got_sequence[expected_size];
+    int expected;
+    int got;
 
+    // TODO: rewrite this so it compares strings instead
     for (int i = 0; i < expected_size; i++)
     {
-        expected_sequence[i] = aminoAcidData.getAminoAcidIndex(expected_sequence_names[i]);
-        got_sequence[i] = molecule.Residues[i].aminoAcidIndex;
+        expected = aminoAcidData.getAminoAcidIndex(expected_sequence_names[i]);
+        got = molecule.Residues[i].aminoAcidIndex;
+        CPPUNIT_ASSERT_EQUAL(expected, got);
     }
+}
 
-    CHECK_ARRAY_EQUAL(expected_sequence, got_sequence, expected_size);
-
+void TestMolecule::testPDBCentre()
+{
     static const float expected_centre[3] = {30.442894736842103, 29.00898684210527, 15.5163947368421};
-    float got_centre[3] = {molecule.center.x, molecule.center.y, molecule.center.z};
 
-    CHECK_ARRAY_CLOSE(expected_centre, got_centre, 3, 0.00001);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(expected_centre[0], molecule.center.x, 0.00001);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(expected_centre[1], molecule.center.y, 0.00001);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(expected_centre[2], molecule.center.z, 0.00001);
 }
