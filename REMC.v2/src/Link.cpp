2c38c3d6ab6cc8051126921c35bddf860b523a57
Adrianna Pińska <adrianna.pinska@gmail.com>
2011-10-31 15:58:57 +0200

    first stab at linker potential functions

--- REMC.v2/src/Link.cpp
+++ REMC.v2/src/Link.cpp
@@ -2,6 +2,19 @@
 
 Link::Link()
 {
+    Angle = 0.0;
+    BondLength = 0.0;
+    TorsionAngle = 0.0;
+
+    e_bond = 0.0;
+    e_angle = 0.0;
+    e_torsion = 0.0;
+
+    update_e_bond = true;
+    update_e_angle = true;
+    update_e_torsion = true;
+
+    flexible = false;
     terminal = false;
 }
 
@@ -14,6 +27,15 @@ Link::Link(const Link & l)
     Angle = l.Angle;
     BondLength = l.BondLength;
     TorsionAngle = l.TorsionAngle;
+
+    e_bond = l.e_bond;
+    e_angle = l.e_angle;
+    e_torsion = l.e_torsion;
+
+    update_e_bond = l.update_e_bond;
+    update_e_angle = l.update_e_angle;
+    update_e_torsion = l.update_e_torsion;
+
     flexible = l.flexible;
     terminal = l.terminal;
 }
--- REMC.v2/src/Link.h
+++ REMC.v2/src/Link.h
@@ -10,9 +10,27 @@ public:
     Link operator = (Link l);
 
     //private:
+    // TODO: these will probably be deprecated in favour of cached potential components
     float Angle;  // angular displacement relative to previous bead
     float TorsionAngle; // twist relative to previous bead
     float BondLength; // distance between this and the previous bead
+
+    // Cached energy potential values
+
+    /* Bond-stretching potential for this link, before multiplication by
+     * (0.5 * K_spring), which is performed on the final sum over all links. */
+    float e_bond;
+    /* Angle potential for the previous residue, before natural log and division
+     * by gamma angle, which are performed on the final product over all links.
+     * TODO: consider moving this to the residue object. */
+    float e_angle;
+    float e_torsion; // torsion-angle potential for this link
+
+    // These are set to 1 if local MC moves necessitate an update of the cached values
+    bool update_e_bond;
+    bool update_e_angle;
+    bool update_e_torsion;
+
     float flexible; // 0 (inflexible) or 1 (flexible): determines fixed tertiary structures
     bool terminal; // true if a new chain starts from this link;
 };
