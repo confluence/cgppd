d7bc8a605fd083061f1fa847880826610ea4f969
Adrianna Pińska <adrianna.pinska@gmail.com>
2011-12-20 11:33:29 +0200

    started kahan tuple polymorphism refactoring; broken

--- REMC.v2/src/Potential.h
+++ REMC.v2/src/Potential.h
@@ -7,52 +7,90 @@
 #include <cmath>
 #include <iostream>
 
+#if COMPENSATE_KERNEL_SUM
+struct KahanTuple
+{
+    double sum;
+    double c;
+
+    KahanTuple();
+    ~KahanTuple();
+};
+#endif // COMPENSATE_KERNEL_SUM
+
 class Potential
 {
 private:
-    double c_lj;
-    double c_dh;
-#if FLEXIBLE_LINKS
-    double c_bond;
-    double c_torsion;
-#endif
-
-    void kahan_sum(double &sum, const double i, double &c);
-    void sum(double &sum, const double i, double &c);
+#if COMPENSATE_KERNEL_SUM
+    void kahan_sum(KahanTuple &sum, const double i);
+    void sum(KahanTuple &sum, const double i);
+#endif // COMPENSATE_KERNEL_SUM
+    void sum(double &sum, const double i);
 
 public:
+
+#if COMPENSATE_KERNEL_SUM
+    KahanTuple LJ;
+    KahanTuple DH;
+#else // if not COMPENSATE_KERNEL_SUM
     double LJ;
     double DH;
+#endif // COMPENSATE_KERNEL_SUM
+
 #if FLEXIBLE_LINKS
-    double bond;
     double angle;
+#if COMPENSATE_KERNEL_SUM
+    KahanTuple bond;
+    KahanTuple torsion;
+#else // if not COMPENSATE_KERNEL_SUM
+    double bond;
     double torsion;
-#endif
+#endif // COMPENSATE_KERNEL_SUM
+#endif // FLEXIBLE_LINKS
 
     Potential();
     ~Potential();
 
+    /* Getters */
+    double get_LJ();
+    double get_DH();
+#if FLEXIBLE_LINKS
+    double get_bond();
+    double get_angle();
+    double get_torsion();
+#endif // FLEXIBLE_LINKS
+
     /* Increment components individually after calculating */
     void increment_LJ(Residue &ri, Residue &rj, const double r, const AminoAcids &AminoAcidsData);
     void increment_DH(Residue &ri, Residue &rj, const double r);
-    void increment_LJ(Residue &ri, Residue &rj, const double r, const AminoAcids &AminoAcidsData, double &sum, double &c);
-    void increment_DH(Residue &ri, Residue &rj, const double r, double &sum, double &c);
+#if COMPENSATE_KERNEL_SUM
+    void increment_LJ(Residue &ri, Residue &rj, const double r, const AminoAcids &AminoAcidsData, KahanTuple &sum);
+    void increment_DH(Residue &ri, Residue &rj, const double r, KahanTuple &sum);
+#else // if not COMPENSATE_KERNEL_SUM
+    void increment_LJ(Residue &ri, Residue &rj, const double r, const AminoAcids &AminoAcidsData, double &sum);
+    void increment_DH(Residue &ri, Residue &rj, const double r, double &sum);
+#endif // COMPENSATE_KERNEL_SUM
 #if FLEXIBLE_LINKS
     void increment_bond(Residue &ri, Link &l, Residue &rj, const float bounding_value);
     void increment_angle(Residue &rh, Residue &ri, Residue &rj);
     void increment_torsion(Residue &rh, Residue &ri, Link &l, Residue &rj, Residue &rk, TorsionalLookupMatrix &torsions);
-#endif
+#endif // FLEXIBLE_LINKS
 
     /* Increment components individually*/
     void increment_LJ(const double LJ);
     void increment_DH(const double DH);
-    void increment_LJ(double &sum, const double LJ, double &c);
-    void increment_DH(double &sum, const double DH, double &c);
+#if COMPENSATE_KERNEL_SUM
+    void increment_LJ(KahanTuple &sum, const double LJ);
+    void increment_DH(KahanTuple &sum, const double DH);
+#else // if not COMPENSATE_KERNEL_SUM
+    void increment_LJ(double &sum, const double LJ);
+    void increment_DH(double &sum, const double DH);
+#endif // COMPENSATE_KERNEL_SUM
 #if FLEXIBLE_LINKS
     void increment_bond(const double bond);
     void increment_angle(const double angle);
     void increment_torsion(const double torsion);
-#endif
+#endif // FLEXIBLE_LINKS
 
     /* Increment all components */
     void increment(const Potential p);
@@ -64,7 +102,7 @@ public:
     double total_bond();
     double total_angle();
     double total_torsion();
-#endif
+#endif // FLEXIBLE_LINKS
 
     /* Output final total */
     double total();
