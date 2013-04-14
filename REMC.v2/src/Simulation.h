074d211e7c39c3fe34c09cfbf62af7c062d8bc07
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2013-04-15 01:48:52 +0200

    refactored main to put most stuff in Simulation; everything probably horribly broken.

--- REMC.v2/src/Simulation.h
+++ REMC.v2/src/Simulation.h
@@ -1,13 +1,76 @@
 #ifndef SIMULATION_H_
 #define SIMULATION_H_
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <gsl/gsl_qrng.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 #include "definitions.h"
+#include "AminoAcid.h"
+#include "TorsionalLookupMatrix.h"
+#include "Replica.h"
+#include "vector3f.h"
+#include "Quaternion.h"
+#include <map>
+#include <unistd.h>  // include for profiling, TAU cannot find getpid() as it is an externed call
+
+#if USING_CUDA
+#include <cuda.h>
+#include <cutil.h>  // CUDA c util package
+#include <cuda_runtime_api.h>
+#include <cutil_inline.h>
+#include <cutil_inline_runtime.h>
+#include "cudaExterns.h"
+#endif
+
+#if GLVIS
+#include "openglvis.h"
+#endif
 
 class Simulation
 {
 public:
-    Simulation();
+    #if GLVIS
+    Replica *GLreplica;
+    #endif
+
+    AminoAcids aminoAcidData;
+
+    float e = 2.71828182845904523536f;
+    size_t lowestEnergy;
+    Replica initialReplica;
+    Replica replica[REPLICA_COUNT];   // container for all replicas in the simulation
+    bool replicasInitialised = false;
+
+    // TODO: create one rng; pass into replicas! Thesis assumes one rng for all replicas.
+    gsl_rng  * REMCRng;         // random numbers for the REMC method to swap labels
+
+    // required objects for synchronisation
+    pthread_mutex_t waitingThreadMutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t waitingCounterMutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t reMutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t writeFileMutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_cond_t waitingThreadCond;
+    pthread_cond_t waitingReplicaExchangeCond;
+    int waitingThreadCount;
+
+    Simulation(argdata parameters);
     ~Simulation();
+
+    void run_check();
+    void init();
+
+    void *MCthreadableFunction(void *arg);
+    void run();
+
+    void initSamplingFiles (FILE * fractionBoundFile, FILE * boundConformationsFile, FILE * acceptanceRatioFile);
+    void closeSamplingFiles (FILE * fractionBoundFile, FILE * boundConformationsFile, FILE * acceptanceRatioFile);
 };
 
 #endif /*SIMULATION_H_*/
--- REMC.v2/src/main.h
+++ REMC.v2/src/main.h
@@ -1,62 +1,9 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <cstring>
-#include <cmath>
-#include <pthread.h>
-#include <sys/mman.h>
-#include <gsl/gsl_qrng.h>
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_randist.h>
-#include "definitions.h"
-#include "AminoAcid.h"
-#include "TorsionalLookupMatrix.h"
-#include "Replica.h"
-#include "vector3f.h"
-#include "Quaternion.h"
-#include <map>
-#include <unistd.h>  // include for profiling, TAU cannot find getpid() as it is an externed call
-
-//all cuda things
-#if USING_CUDA
-#include <cuda.h>
-#include <cutil.h>  // CUDA c util package
-#include <cuda_runtime_api.h>
-#include <cutil_inline.h>
-#include <cutil_inline_runtime.h>
-#include "cudaExterns.h"
-
-int cuda_blockSize = TILE_DIM;
-bool auto_blockdim = true;
-
-#endif
+#ifndef MAIN_H_
+#define MAIN_H_
+#include "Simulation.h"
 
 using namespace std;
 
-float e = 2.71828182845904523536f;
-size_t lowestEnergy;
-Replica replica[REPLICA_COUNT];   // container for all replicas in the simulation
-vector<uint> nonCrowderMolecules; // contains a list of each molecule/protein we want, lets us quickly determine which ones to save
-
-// TODO: create one rng; pass into replicas! Thesis assumes one rng for all replicas.
-gsl_rng  * REMCRng;         // random numbers for the REMC method to swap labels
-bool exitCondition = false;
-bool viewConditions = false;
-bool skipsimulation = false;
-int mcstepcount = 0;
-int threads = THREAD_COUNT;   // 2 threads per core i think
-int streams = STREAM_COUNT;
-
-void REMCSimulation(Replica *initialReplica, argdata *parameters);
-bool getArgs(argdata * d, int argc, char **argv);
 void printHelp(bool badArg); // TODO: wrapper for bad args + help; rename to usage
+bool getArgs(argdata * d, int argc, char **argv);
 void loadArgsFromFile(argdata * parameters);
-void *MCthreadableFunction(void *arg);
-void initSamplingFiles (argdata *args, FILE * fractionBoundFile, FILE * boundConformationsFile, FILE * acceptanceRatioFile);
-void closeSamplingFiles (argdata *args, FILE * fractionBoundFile, FILE * boundConformationsFile, FILE * acceptanceRatioFile);
-
-// All the opengl stuff
-#if GLVIS
-Replica *GLreplica;
-#include "openglvis.h"
-#endif
