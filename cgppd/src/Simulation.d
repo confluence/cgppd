f2c756c5b3dc65df59c059cfaf99deef8ca7d667
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2013-05-06 14:45:59 +0200

    this shouldn't be version controlled

--- cgppd/src/Simulation.d
+++
@@ -1,5 +0,0 @@
-obj/Simulation.o: src/Simulation.cpp src/Simulation.h src/definitions.h \
- src/structures.h src/AminoAcid.h src/TorsionalLookupMatrix.h \
- src/AminoAcids.h src/Replica.h src/vector3f.h src/Quaternion.h \
- src/Molecule.h src/Residue.h src/Link.h src/Segment.h src/Potential.h \
- src/constants.h
