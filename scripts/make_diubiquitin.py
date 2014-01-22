2d08fa7cbcde19cafab8e2cb0100701eb1aa2672
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2014-01-22 17:22:58 +0200

    updated notes

--- scripts/make_diubiquitin.py
+++ scripts/make_diubiquitin.py
@@ -6,6 +6,5 @@ import numpy as np
 # draw line along tail of second ubiquitin
 # rotate second ubiquitin about tail end so that lines are parallel
     # https://stackoverflow.com/questions/10161553/rotate-a-vector-to-reach-another-vector ?
-# translate it so that the end of the tail is 8.8 A away from the desired residue
-# does it matter where the centre of the rigid domain is?
+# translate it so that the end of the tail is 8.8 A away from the desired residue along the line
 # output as single pdb or two pdbs
