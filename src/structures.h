1b413e1395a8b67f59f3f9ba0fa35ee110fce8b3
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2014-02-27 16:54:32 +0200

    more planning in notes; basic geometry classes

--- src/structures.h
+++ src/structures.h
@@ -1,131 +1,169 @@
 #ifndef STRUCTURES_H_
-    #define STRUCTURES_H_
+#define STRUCTURES_H_
 
-    #include <vector>
-    #include <map>
-    #include <iostream>
-    #include <string.h>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <string.h>
 
-    #include "definitions.h"
+#include "definitions.h"
 
-    using namespace std;
+using namespace std;
 
-    class Replica;
+class Replica;
 
-    struct SimulationData
-    {
-        long index;
-        Replica *replica;
-        int replicaCount;
-        int GPUID;
-        int threads;
-        int streams;
-        int replicas_in_this_thread;
-        int max_replicas_per_thread;
-        int replicas_per_stream;
-        int streams_per_thread;
-        int MCsteps;
-        int REsteps; // TODO: will this ever be used in the threadable function if we precalculate mcstepsPerRE?
-        int MC_steps_per_RE;
-        int sampleFrequency;
-        int sampleStartsAfter;
-        int checkpointFrequency;
-        float bound;
-        int * waitingThreadCount;
-        char prefix[256];
-        char title[256];
-        FILE * fractionBound;
-        FILE * boundConformations;
-        pthread_mutex_t * waitingCounterMutex;
-        pthread_mutex_t * writeFileMutex;
+struct SimulationData
+{
+    long index;
+    Replica *replica;
+    int replicaCount;
+    int GPUID;
+    int threads;
+    int streams;
+    int replicas_in_this_thread;
+    int max_replicas_per_thread;
+    int replicas_per_stream;
+    int streams_per_thread;
+    int MCsteps;
+    int REsteps; // TODO: will this ever be used in the threadable function if we precalculate mcstepsPerRE?
+    int MC_steps_per_RE;
+    int sampleFrequency;
+    int sampleStartsAfter;
+    int checkpointFrequency;
+    float bound;
+    int * waitingThreadCount;
+    char prefix[256];
+    char title[256];
+    FILE * fractionBound;
+    FILE * boundConformations;
+    pthread_mutex_t * waitingCounterMutex;
+    pthread_mutex_t * writeFileMutex;
 #ifdef VERBOSE_THREAD_LOGGING
-        pthread_mutex_t * logMutex;
+    pthread_mutex_t * logMutex;
 #endif
-        pthread_cond_t * waitingThreadCond;
-        pthread_cond_t * waitingReplicaExchangeCond;
-    };
+    pthread_cond_t * waitingThreadCond;
+    pthread_cond_t * waitingReplicaExchangeCond;
+};
 
-    struct segdata
+struct segdata
+{
+    vector<int> residue_indices;
+};
+
+struct moldata
+{
+    char pdbfilename[256];
+    char name[256];
+    float px;
+    float py;
+    float pz;
+    bool translate;
+    float rx;
+    float ry;
+    float rz;
+    float ra;
+    bool crowder;
+    bool all_flexible;
+
+    vector<segdata> segments;
+
+    moldata(): px(0), py(0), pz(0), translate(false), rx(0), ry(0), rz(0), ra(0), crowder(false), all_flexible(false)
     {
-        vector<int> residue_indices;
-    };
+        memset(pdbfilename, 0, 256);
+        memset(name, 0, 256);
+    }
+};
 
-    struct moldata
-    {
-        char pdbfilename[256];
-        char name[256];
-        float px;
-        float py;
-        float pz;
-        bool translate;
-        float rx;
-        float ry;
-        float rz;
-        float ra;
-        bool crowder;
-        bool all_flexible;
-
-        vector<segdata> segments;
-
-        moldata(): px(0), py(0), pz(0), translate(false), rx(0), ry(0), rz(0), ra(0), crowder(false), all_flexible(false)
-        {
-            memset(pdbfilename, 0, 256);
-            memset(name, 0, 256);
-        }
-    };
-
-    struct argdata
-    {
-        bool viewConditions;
-        bool skipsimulation;
+struct argdata
+{
+    bool viewConditions;
+    bool skipsimulation;
 #if USING_CUDA
-        int cuda_blockSize;
-        bool auto_blockdim;
+    int cuda_blockSize;
+    bool auto_blockdim;
 #endif
-        int threads;
-        int streams;
-        int max_replicas_per_thread;
-        int gpus;
-        int MCsteps;
-        int REsteps;
-        int replicas;
-        float bound;
-        int sampleFrequency;
-        int sampleStartsAfter;
-        int checkpointFrequency;
-        char prefix[256];
-        char title[256];
-        char file[256];
-        char logfile[256];
-        bool inputFile;
+    int threads;
+    int streams;
+    int max_replicas_per_thread;
+    int gpus;
+    int MCsteps;
+    int REsteps;
+    int replicas;
+    float bound;
+    int sampleFrequency;
+    int sampleStartsAfter;
+    int checkpointFrequency;
+    char prefix[256];
+    char title[256];
+    char file[256];
+    char logfile[256];
+    bool inputFile;
 //         TODO: for some reason I wanted to remove this
-        int nonCrowders;
-        float temperatureMin;
-        float temperatureMax;
-        int pid;
-        // TODO: this is gone until checkpointing goes back in
+    int nonCrowders;
+    float temperatureMin;
+    float temperatureMax;
+    int pid;
+    // TODO: this is gone until checkpointing goes back in
 //         char checkpointfilename[256];
-        bool resume;
-        vector<moldata> mdata;
-        map<string, int> mdata_map;
-        int verbosity;
+    bool resume;
+    vector<moldata> mdata;
+    map<string, int> mdata_map;
+    int verbosity;
 
-        argdata() :
-            nonCrowders(0), viewConditions(false), skipsimulation(false),
+    argdata() :
+        nonCrowders(0), viewConditions(false), skipsimulation(false),
 #if USING_CUDA
-            cuda_blockSize(TILE_DIM), auto_blockdim(true),
+        cuda_blockSize(TILE_DIM), auto_blockdim(true),
 #endif
-            gpus(1), threads(THREAD_COUNT), streams(STREAM_COUNT), MCsteps(MC_STEPS), REsteps(REMC_STEPS),
-            sampleFrequency(SAMPLE_FREQ), sampleStartsAfter(STEPS_BEFORE_SAMPLE), inputFile(false),
-            replicas(REPLICA_COUNT), bound(BOUNDING_VALUE), temperatureMin(LOWTEMP), temperatureMax(HIGHTEMP),
-            pid(0), resume(false), checkpointFrequency(CHECKPOINTFREQUENCY), max_replicas_per_thread(0),
-            verbosity(0)
-        {
-            memset(prefix, 0, 256);
-            memset(file, 0, 256);
-            memset(logfile, 0, 256);
-            memset(title, 0, 256);
-        }
-    };
+        gpus(1), threads(THREAD_COUNT), streams(STREAM_COUNT), MCsteps(MC_STEPS), REsteps(REMC_STEPS),
+        sampleFrequency(SAMPLE_FREQ), sampleStartsAfter(STEPS_BEFORE_SAMPLE), inputFile(false),
+        replicas(REPLICA_COUNT), bound(BOUNDING_VALUE), temperatureMin(LOWTEMP), temperatureMax(HIGHTEMP),
+        pid(0), resume(false), checkpointFrequency(CHECKPOINTFREQUENCY), max_replicas_per_thread(0),
+        verbosity(0)
+    {
+        memset(prefix, 0, 256);
+        memset(file, 0, 256);
+        memset(logfile, 0, 256);
+        memset(title, 0, 256);
+    }
+};
+
+// GEOMETRY FOR FLEXIBLE LINKERS
+
+struct Bond
+{
+    // residue indices
+    int i;
+    int j;
+
+    // cache for tests
+    double length;
+    double potential;
+};
+
+struct Angle
+{
+    // residue indices
+    int i;
+    int j;
+    int k;
+
+    // cache for tests
+    double theta;
+    double potential;
+};
+
+struct Torsion
+{
+    // residue indices
+    int i;
+    int j;
+    int k;
+    int l;
+
+    // cache for tests
+    double phi;
+    double potential;
+};
 
 #endif
