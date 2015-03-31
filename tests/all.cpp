ff1ccbe103b443123d7f6816f5137ad582e5449d
Adrianna Pińska <adrianna.pinska@gmail.com>
2015-03-31 13:26:20 +0200

    removed some commented-out crap

--- tests/all.cpp
+++ tests/all.cpp
@@ -9,10 +9,8 @@ using namespace std;
 
 int main( int argc, char **argv)
 {
-    //FLAGS_log_dir = "logs";
-    //FLAGS_alsologtostderr = 1;
-    //google::InitGoogleLogging(argv[0]);
-
+    START_EASYLOGGINGPP(argc, argv);
+    
     CppUnit::TextUi::TestRunner runner;
     CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
     runner.addTest( registry.makeTest() );
