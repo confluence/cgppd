b68ea00cca59ebbd4d2b553d91f24eb0cf713d87
Adrianna Pińska <adrianna.pinska@gmail.com>
2015-03-31 20:42:00 +0200

    reference conformation test and removed download script for external dependencies because it has been replaced by makefile rules

--- inc/download_external.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/easylogging/easyloggingpp/releases/download/v9.80/easyloggingpp_v9.80.tar.gz && tar -xzf easyloggingpp_v9.80.tar.gz && rm easyloggingpp_v9.80.tar.gz && chmod -x "easylogging++.h"
-
-wget https://raw.githubusercontent.com/philsquared/Catch/develop/single_include/catch.hpp
--- tests/testCommon.cpp
+++ tests/testCommon.cpp
@@ -1,63 +1,5 @@
 #include <testCommon.h>
 
-//void check_vector3f_equal(Vector3f expected, Vector3f actual, CppUnit::SourceLine sourceLine)
-//{
-    //if (expected.almost_equal(actual, 0.00001)) {
-        //return;
-    //}
-    
-    //ostringstream s_expected;
-    //ostringstream s_actual;
-    
-    //s_expected << expected;
-    //s_actual << actual;
-    
-    //::CppUnit::Asserter::failNotEqual(s_expected.str(), s_actual.str(), sourceLine);
-//}
-
-//void check_potential_equal(Potential expected, Potential actual, CppUnit::SourceLine sourceLine)
-//{
-    //if (expected.almost_equal(actual, 0.00001)) {
-        //return;
-    //}
-    
-    //ostringstream s_expected;
-    //ostringstream s_actual;
-    
-    //s_expected << expected;
-    //s_actual << actual;
-    
-    //::CppUnit::Asserter::failNotEqual(s_expected.str(), s_actual.str(), sourceLine);
-//}
-
-//template <>
-//string join(const string sep, set<int> values)
-//{
-    //string s;
-
-    //for (typename set<int>::iterator i = values.begin(); i != values.end(); i++) {
-        //char temp[11];
-        //sprintf(temp, "%d", *i);
-        //s += sep + string(temp);
-    //}
-
-    //return s;
-//}
-
-//template <>
-//string join(const string sep, vector<int> values)
-//{
-    //string s;
-
-    //for (typename vector<int>::iterator i = values.begin(); i != values.end(); i++) {
-        //char temp[11];
-        //sprintf(temp, "%d", *i);
-        //s += sep + string(temp);
-    //}
-
-    //return s;
-//}
-
 vector<int> make_range(int begin, int end)
 {
     vector<int> v;
@@ -66,7 +8,6 @@ vector<int> make_range(int begin, int end)
     }
     return v;
 }
-
 
 set<int> to_set(vector<int> v)
 {
