0f9ef1c8085d0ea4acca8ffbb6b18cb2f39b7b2f
Adrianna Pińska <adrianna.pinska@gmail.com>
2015-04-23 10:50:06 +0200

    changed these to use a single replica

--- scripts/polyalanine_simulations_ljoff.sh
+++ scripts/polyalanine_simulations_ljoff.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 make clean
-make LJ=off STREAMS=yes
+make LJ=off STREAMS=yes POLYMERTEST=yes
 
 for n in 4 8 16 32 64 128 256 512 1024 2048
 do
-    ./cgppd_ljoff -f config/ala$n
+    ./cgppd_ljoff -r 1 -x 300 -n 300 -f config/ala$n
 done
 
 # TODO: add processing (pdbstats?)
--- scripts/polyalanine_simulations_ljrep.sh
+++ scripts/polyalanine_simulations_ljrep.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 make clean
-make LJ=repulsive STREAMS=yes
+make LJ=repulsive STREAMS=yes POLYMERTEST=yes
 
 for n in 4 8 16 32 64 128 256 512 1024 2048
 do
-    ./cgppd_ljrep -f config/ala$n
+    ./cgppd_ljrep -r 1 -x 300 -n 300 -f config/ala$n
 done
 
 # TODO: add processing (pdbstats?)
