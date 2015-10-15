11dc2022f212fc6b98ad4f667bae291797e9c437
Adrianna Pińska <adrianna.pinska@gmail.com>
2015-10-15 16:42:09 +0200

    meeting and updtaed script

--- scripts/stupid_distance_histogram_demo.py
+++ scripts/stupid_distance_histogram_demo.py
@@ -5,13 +5,30 @@
 import sys
 import matplotlib.pyplot as plt
 from collections import Counter
+import numpy as np
 
-things = [float(arg) for arg in sys.argv[1:]]
+things = []
+for arg in sys.argv[1:]:
+    strs = arg.split(',')
+    things.append(np.array([float(n) for n in strs]))
+
+#things = [np.array(arg.split(",")) for arg in sys.argv[1:]]
 pairs = [(t1, t2) for i, t1 in enumerate(things) for t2 in things[i+1:]]
-dists = [abs(t1 - t2) for (t1, t2) in pairs]
-rounded_dists = [int(d) for d in dists]
-hist = Counter(rounded_dists)
 
-x, y = zip(*sorted(hist.items()))
-plt.bar(x, y)
+
+
+#dists = [abs(t1 - t2) for (t1, t2) in pairs]
+#rounded_dists = [int(d) for d in dists]
+
+dists = [np.linalg.norm(t1 - t2) for (t1, t2) in pairs]
+
+plt.hist(dists)
 plt.show()
+
+# do proper histogram here
+
+#hist = Counter(rounded_dists)
+
+#x, y = zip(*sorted(hist.items()))
+#plt.bar(x, y)
+#plt.show()
