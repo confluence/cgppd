3065d26a61d0b253631510443ed4498f46e3b49b
Adrianna Pińska <adrianna.pinska@gmail.com>
2015-10-22 17:01:49 +0200

    pick a temperature

--- scripts/create_trajectory.sh
+++ scripts/create_trajectory.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-cat $1/pdb/*300.0K* >> $1/trajectory.pdb
-sed -i 's/END/ENDMDL/' $1/trajectory.pdb
+if [ -z "$2" ]
+then
+    temp=300.0
+else
+    temp=$2
+fi
+
+if test -n "$(find $1/pdb -maxdepth 1 -name *${temp}K* -print -quit)"
+then
+    cat $1/pdb/*${temp}K* >> $1/trajectory.pdb
+    sed -i 's/END/ENDMDL/' $1/trajectory.pdb
+else
+    echo "No samples found at temperature ${temp}K."
+fi
+
+
+
+
+
