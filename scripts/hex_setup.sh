abe2469fe6ab2d8f66fb59236449727401b5a650
Adrianna Pińska <adrianna.pinska@gmail.com>
2015-11-02 13:58:06 +0200

    another env variable

--- scripts/hex_setup.sh
+++ scripts/hex_setup.sh
@@ -1,5 +1,8 @@
 module add cuda/Cuda-6.5
 module add compilers/gcc484
+# Add samples
 export INCLUDE=/usr/local/cuda-6.5/samples/common/inc:$INCLUDE
 export LD_LIBRARY_PATH=/usr/local/cuda-6.5/samples/common/lib:$LD_LIBRARY_PATH
+# I don't know why these aren't set automatically, but we need them
 export CPATH=$INCLUDE
+export LIBRARY_PATH=$LD_LIBRARY_PATH
