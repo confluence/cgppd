c154aa5f811d2acbd1aae60e590251546068dcd4
apinska <apinska@cs.uct.ac.za>
2015-11-03 00:54:46 +0200

    more jobs

---
+++ jobs/diubiquitin-4cores.sh
@@ -0,0 +1,13 @@
+#PBS -q GPUQ
+#PBS -l nodes=srvslsgpu003:ppn=4:seriesGPUk
+#PBS -N diubiquitin
+#PBS -V
+
+cd /home/apinska/repos/cgppd
+
+source scripts/hex_setup.sh
+
+for configfile in config/diubiquitin_lys_48  config/diubiquitin_lys_63  config/diubiquitin_met_1
+do
+    ./cgppd -f $configfile -t 4 -s 4 --gpuoffset 1
+done
---
+++ jobs/nothing.sh
@@ -0,0 +1,6 @@
+#PBS -q GPUQ
+#PBS -l nodes=srvslsgpu003:ppn=1:seriesGPUk
+#PBS -N interactive
+#PBS -V
+
+echo "Hello, world"
