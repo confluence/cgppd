445172b3c8209187ebdf67e6ab1d8f9abd3f5b69
Adrianna Pińska <adrianna.pinska@gmail.com>
2016-02-25 15:11:43 +0200

    jobs and config for ubq/UIM simulations

---
+++ jobs/ubq_uim_rigid.sh
@@ -0,0 +1,13 @@
+#PBS -q GPUQ
+#PBS -l nodes=srvslsgpu003:ppn=10:seriesGPUk
+#PBS -N ubq_uim_rigid
+#PBS -V
+
+cd /home/apinska/repos/cgppd
+
+source scripts/hex_setup.sh
+
+for configfile in config/ubq_uim_rigid/*
+do
+    ./cgppd -f $configfile -t 20 -s 20 -g 2 -o ubq_uim_rigid/$configfile
+done
