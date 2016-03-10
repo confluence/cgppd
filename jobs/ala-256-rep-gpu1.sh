b32cd5cb4f281a83a8885a6892e9dc25a457cd37
Adrianna Pińska <adrianna.pinska@gmail.com>
2016-03-10 08:20:58 +0200

    forgot to change no. of replicas

--- jobs/ala-256-rep-gpu1.sh
+++ jobs/ala-256-rep-gpu1.sh
@@ -7,4 +7,4 @@ cd /home/apinska/repos/cgppd
 
 source scripts/hex_setup.sh
 
-./cgppd_ljrep -f config/ala256 -t 20 -s 20 -o lj_rep/ala256 -m 40000000 -e 8000 -n 240 -x 420 --gpuoffset 1
+./cgppd_ljrep -f config/ala256 -r 20 -t 20 -s 20 -o lj_rep/ala256 -m 40000000 -e 8000 -n 240 -x 420 --gpuoffset 1
