fc26bb3d842c8126b9b01ad0421379edff458120
Adrianna Pińska <adrianna.pinska@gmail.com>
2016-09-01 16:18:59 +0200

    fret efficiency script

--- scripts/estimate_FRET_efficiency.py
+++ scripts/estimate_FRET_efficiency.py
@@ -7,11 +7,26 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate FRET efficiency from diubiquitin simulations produced by cgppd")
     parser.add_argument("dirs", help="Individual directories to process", nargs="+")
+    parser.add_argument("-r", "--reference-length", help="Set R0 value", type=float, default=50.0)
+
 
     args = parser.parse_args()
 
     simulation_group = DiubiquitinSimulationGroup.from_dirs(args.dirs)
+    R0 = args.reference_length
     
     for (name, sim) in simulation_group.sims:
-        print name
+        print "Simulation %s:" % name
+        
+        for i, cluster in enumerate(sim.clusters, 1):
+            sample_E = []
+            
+            for sample in cluster.samples:
+                R = sample.length
+                E = 1.0 / (1.0 + (R / R0))**6
+                sample_E.append(E)
+                
+            average_E = sum(sample_E) / len(sample_E)
+            
+            print "Cluster %d (%d samples): average E %.3f" % (i, len(sample_E), average_E)
 
