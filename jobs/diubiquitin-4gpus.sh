8a399abdfd032f838c6c0edbb2a3115b7ff405ea
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-03-06 14:31:04 +0200

    mess of new jobs

---
+++ jobs/diubiquitin-4gpus.sh
@@ -0,0 +1,13 @@
+#PBS -q GPUQ
+#PBS -l nodes=srvslsgpu002:ppn=12:seriesGPU
+#PBS -N diubiquitin-2gpus
+#PBS -V
+
+cd /home/apinska/repos/cgppd
+
+source scripts/hex_setup.sh
+
+for configfile in config/diubiquitin_lys_48  config/diubiquitin_lys_63  config/diubiquitin_met_1
+do
+    ./cgppd -f $configfile -t 20 -s 20 -g 4
+done
