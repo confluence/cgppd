2cab2e49e3a7a3cf5701b615c266b747ab23e1dd
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2017-03-07 14:15:18 +0200

    moved two jobs to gpu004

--- jobs/diubiquitin-lys-11.sh
+++ jobs/diubiquitin-lys-11.sh
@@ -1,5 +1,5 @@
 #PBS -q GPUQ
-#PBS -l nodes=srvslsgpu003:ppn=4:seriesGPUk
+#PBS -l nodes=srvslsgpu004:ppn=4:seriesGPUk
 #PBS -N diubiquitin_lys_11
 #PBS -V
 
@@ -8,3 +8,4 @@ cd /home/apinska/repos/cgppd
 source scripts/hex_setup.sh
 
 ./cgppd -f config/diubiquitin_lys_11 -t 4 -s 4
+#./cgppd -f config/diubiquitin_lys_11 -t 4 -s 4 --gpuoffset 2
