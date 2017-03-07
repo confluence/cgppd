d7050efb8107375178a3550ad24244f7c17f0f34
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-03-07 14:13:05 +0200

    sample jobs with no sidechain

---
+++ jobs/diubiquitin-met-1-no-sidechain.sh
@@ -0,0 +1,10 @@
+#PBS -q GPUQ
+#PBS -l nodes=srvslsgpu003:ppn=4:seriesGPUk
+#PBS -N config/diubiquitin_met_1
+#PBS -V
+
+cd /home/apinska/repos/cgppd
+
+source scripts/hex_setup.sh
+
+./cgppd -f config/diubiquitin_met_1 -t 4 -s 4 --gpuoffset 1
