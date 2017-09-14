9b539e18671b89c24eded2447b941caa996900cf
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-09-14 02:01:05 +0200

    moved figures to other repo; updated some vmd stuff

--- scripts/vmd_fragments.tcl
+++ scripts/vmd_fragments.tcl
@@ -2,34 +2,35 @@
 # It probably won't make much difference, but we should be rigorous.
 # Open them, align them, and write them back.
 
-# load the molecules
+# diubiquitin
 
-mol new output/diubiquitin/diubiquitin_2016-05-18_A/diubiquitin_lys_48_6283/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top LYS-48
+mol new output/best_diubiquitin/diubiquitin_lys_48_6283/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol rename top Lys48-diubq
 
-mol new output/diubiquitin/diubiquitin_2016-05-18_A/diubiquitin_lys_63_11433/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top LYS-63
+mol new output/best_diubiquitin/diubiquitin_lys_63_11433/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol rename top Lys63-diubq
 
-mol new output/diubiquitin/diubiquitin_2016-05-18_A/diubiquitin_met_1_8154/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top MET-1
+mol new output/best_diubiquitin/diubiquitin_met_1_8154/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol rename top Met1-diubq
 
-# Other diubiquitin types:
+mol new output/best_diubiquitin/diubiquitin_lys_11_97240/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol rename top Lys11-diubq
 
-mol new output/diubiquitin/diubiquitin_2017_03_08/diubiquitin_lys_11_97240/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top LYS-11
+mol new output/best_diubiquitin/diubiquitin_lys_27_1256/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol rename top Lys27-diubq
 
-mol new output/diubiquitin/diubiquitin_2017_04_18/diubiquitin_lys_27_1256/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top LYS-27
+mol new output/best_diubiquitin/diubiquitin_lys_29_27702/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol rename top Lys29-diubq
 
-mol new output/diubiquitin/diubiquitin_2017_03_08/diubiquitin_lys_29_27702/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top LYS-29
+mol new output/best_diubiquitin/diubiquitin_lys_33_27721/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol rename top Lys33-diubq
 
-mol new output/diubiquitin/diubiquitin_2017_03_08/diubiquitin_lys_33_27721/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top LYS-33
+mol new output/best_diubiquitin/diubiquitin_lys_6_97254/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol rename top Lys6-diubq
 
-mol new output/diubiquitin/diubiquitin_2017_03_08/diubiquitin_lys_6_97254/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top LYS-6
+# tetraubiquitin
 
+# octaubiquitin
 
 # procedure for restoring default representations after clustering deletes them
 
@@ -38,81 +39,49 @@ proc restore_reps {m} {
     
     # molecule bodies
 	mol addrep $m
-	mol modselect $r $m resid 1 to 73 or resid 77
-	mol modstyle $r $m QuickSurf 1.000000 0.500000 1.000000 1.000000
+	mol modselect $r $m all
+	mol modstyle $r $m VDW 0.5 50.0
 	mol modcolor $r $m Chain
-	mol modmaterial $r $m Opaque
+	mol modmaterial $r $m Glass1
     
     set r [expr $r + 1]
 
-    # tails
-	mol addrep $m
-	mol modselect $r $m resid 73 to 77
-	mol modstyle $r $m QuickSurf 1.000000 0.500000 1.000000 1.000000
-	mol modcolor $r $m ColorID 12
-	mol modmaterial $r $m Opaque
-    
-    set r [expr $r + 1]
-
-    # hydrophobic patches
+    # hydrophobic patch around Ile44
 	mol addrep $m
 	mol modselect $r $m resid 8 or resid 44 or resid 68 or resid 70
-	mol modstyle $r $m QuickSurf 1.200000 0.500000 1.000000 1.000000
-	mol modcolor $r $m ColorID 13
+	mol modstyle $r $m VDW 1.0 50.0
+	mol modcolor $r $m ColorID 0
 	mol modmaterial $r $m Opaque
+
     mol showrep $m $r 0
     
     set r [expr $r + 1]
 
-    # only his-68
+    # hydrophobic patch around Ile44
 	mol addrep $m
-	mol modselect $r $m resid 68
-	mol modstyle $r $m QuickSurf 1.200000 0.500000 1.000000 1.000000
-	mol modcolor $r $m ColorID 10
-	mol modmaterial $r $m Opaque
+	mol modselect $r $m resid 44
+	mol modstyle $r $m VDW 1.1 50.0
+	mol modcolor $r $m ColorID 0
+	mol modmaterial $r $m Diffuse
     
     set r [expr $r + 1]
 
-    # only val-70
+    # hydrophobic patch around Ile36
 	mol addrep $m
-	mol modselect $r $m resid 70
-	mol modstyle $r $m QuickSurf 1.200000 0.500000 1.000000 1.000000
-	mol modcolor $r $m ColorID 3
+	mol modselect $r $m resid 36 or resid 71 or resid 73
+	mol modstyle $r $m VDW 1.0 50.0
+	mol modcolor $r $m ColorID 7
 	mol modmaterial $r $m Opaque
+
+    mol showrep $m $r 0
     
     set r [expr $r + 1]
-    
-    # N terminus
+
+    # hydrophobic patch around Ile36
 	mol addrep $m
-	mol modselect $r $m index 0
-	mol modstyle $r $m QuickSurf 1.200000 0.500000 1.000000 1.000000
-	mol modcolor $r $m ColorID 19
-	mol modmaterial $r $m Opaque
-    
-    set r [expr $r + 1]
-    
-    # C terminus
-	mol addrep $m
-	mol modselect $r $m index 152
-	mol modstyle $r $m QuickSurf 1.200000 0.500000 1.000000 1.000000
-	mol modcolor $r $m ColorID 29
-	mol modmaterial $r $m Opaque
+	mol modselect $r $m resid 36
+	mol modstyle $r $m VDW 1.1 50.0
+	mol modcolor $r $m ColorID 7
+	mol modmaterial $r $m Diffuse
 }
 
-restore_reps 0
-restore_reps 1
-restore_reps 2
-restore_reps 3
-restore_reps 4
-restore_reps 5
-restore_reps 6
-restore_reps 7
-
-mol on 0
-mol off 1
-mol off 2
-mol off 3
-mol off 4
-mol off 5
-mol off 6
-mol off 7
