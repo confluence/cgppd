f83c2b95ffa7e21ade4555be57f345a12d8080fc
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2017-09-16 02:17:25 +0200

    stripped out offsets

--- jobs/diubiquitin-lys-63-moreflex.sh
+++ jobs/diubiquitin-lys-63-moreflex.sh
@@ -7,4 +7,4 @@ cd /home/apinska/repos/cgppd
 
 source scripts/hex_setup.sh
 
-./cgppd -f config/diubiquitin_lys_63_longtail_loop -t 4 -s 4 --gpuoffset 1
+./cgppd -f config/diubiquitin_lys_63_longtail_loop -t 4 -s 4
--- jobs/diubiquitin-met-1.sh
+++ jobs/diubiquitin-met-1.sh
@@ -7,4 +7,4 @@ cd /home/apinska/repos/cgppd
 
 source scripts/hex_setup.sh
 
-./cgppd -f config/diubiquitin_met_1 -t 4 -s 4 --gpuoffset 2
+./cgppd -f config/diubiquitin_met_1 -t 4 -s 4
