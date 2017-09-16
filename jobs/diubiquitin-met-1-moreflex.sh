f7d2bf378be198d0f8d54a884f2969b6e0def462
Adrianna Pińska <adrianna.pinska@gmail.com>
2017-09-16 02:26:48 +0200

    reassigned offsets

--- jobs/diubiquitin-met-1-moreflex.sh
+++ jobs/diubiquitin-met-1-moreflex.sh
@@ -3,8 +3,8 @@
 #PBS -N 2ubq_met_1_ll
 #PBS -V
 
-cd /home/apinska/repos/cgppd
+cd /home/apinska/repos/cgppd --gpu-offset 1
 
 source scripts/hex_setup.sh
 
-./cgppd -f config/diubiquitin_met_1_longtail_loop -t 4 -s 4
+./cgppd -f config/diubiquitin_met_1_longtail_loop -t 4 -s 4 --gpu-offset 1
