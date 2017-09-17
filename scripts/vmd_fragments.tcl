d4b9317b735f0a0109bb9907ebb9cd05de842e9e
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2017-09-17 16:04:49 +0200

    this is now an actual usable script

--- scripts/vmd_fragments.tcl
+++ scripts/vmd_fragments.tcl
@@ -1,36 +1,20 @@
+# Add polyubiquitin directory locations
+
 # diubiquitin
 
-mol new output/best_diubiquitin/diubiquitin_lys_48_6283/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys48-diubq
-
-mol new output/best_diubiquitin/diubiquitin_lys_63_11433/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys63-diubq
-
-mol new output/best_diubiquitin/diubiquitin_met_1_8154/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Met1-diubq
-
-mol new output/best_diubiquitin/diubiquitin_lys_11_97240/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys11-diubq
-
-mol new output/best_diubiquitin/diubiquitin_lys_27_1256/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys27-diubq
-
-mol new output/best_diubiquitin/diubiquitin_lys_29_27702/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys29-diubq
-
-mol new output/best_diubiquitin/diubiquitin_lys_33_27721/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys33-diubq
-
-mol new output/best_diubiquitin/diubiquitin_lys_6_97254/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys6-diubq
+set ubq(diubqLys48) "best_diubiquitin/diubiquitin_lys_48_6283"
+set ubq(diubqLys63) "best_diubiquitin/diubiquitin_lys_63_11433"
+set ubq(diubqMet1) "best_diubiquitin/diubiquitin_met_1_8154"
+set ubq(diubqLys11) "best_diubiquitin/diubiquitin_lys_11_97240"
+set ubq(diubqLys27) "best_diubiquitin/diubiquitin_lys_27_1256"
+set ubq(diubqLys29) "best_diubiquitin/diubiquitin_lys_29_27702"
+set ubq(diubqLys33) "best_diubiquitin/diubiquitin_lys_33_27721"
+set ubq(diubqLys6) "best_diubiquitin/diubiquitin_lys_6_97254"
 
 # tetraubiquitin
-
-mol new output/tetraubiquitin/tetraubiquitin_lys_48_98513/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys48-tetraubq
-
-mol new output/tetraubiquitin/tetraubiquitin_lys_63_104466/trajectory.pdb type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol rename top Lys63-tetraubq
+set ubq(tetraubqLys48) "tetraubiquitin/tetraubiquitin_lys_48_98513"
+set ubq(tetraubqLys63) "tetraubiquitin/tetraubiquitin_lys_63_104466"
+set ubq(tetraubqMet1) "tetraubiquitin/tetraubiquitin_met_1_14451"
 
 # octaubiquitin
 
@@ -98,3 +82,12 @@ proc restore_reps {m} {
     mol showrep $m $r 0
 }
 
+# load a molecule
+
+proc load_ubq {name} {
+    set path "$::env(HOME)/repos/cgppd/output/$::ubq($name)/trajectory.pdb"
+    mol new $path type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+    mol rename top $name
+    mol delrep 0 top
+    restore_reps top
+}
