225302b7ec50276428e09e61288ccb1a126749d4
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2018-01-10 16:49:00 +0200

    a lot more vmd code

--- scripts/vmd_fragments.tcl
+++ scripts/vmd_fragments.tcl
@@ -135,13 +135,21 @@ proc load_ubq {name} {
     mol rename top $name
     mol delrep 0 top
     restore_reps top
+    
+    color Chain A yellow
+    color Chain B orange
+    
     if {[string match "diubq*" $name]} {
         align top "chain A"
     } elseif {[string match "tetraubq*" $name]} {
         align top "chain B or chain C"
+        color Chain C orange2
+        color Chain D red
     } elseif {[string match "octaubq*" $name]} {
         align top "chain C or chain D or chain E or chain F"
     }
+    
+    
     display resetview
 }
 
@@ -156,7 +164,195 @@ display aoambient 0.8
 display aodirect 0.3
 display shadows on
 
-color Chain A yellow
-color Chain B orange
-color Chain C orange2
-color Chain D red
+#                               
+
+# comparison structures
+
+set ubq_ref(diubq1) { "2w9n-AB.pdb" "3axc-AB.pdb" }
+set ubq_ref(diubq6) { "2xk5.pdb" }
+set ubq_ref(diubq11) { "2xew-AB.pdb" "3nob-AB.pdb" }
+set ubq_ref(diubq27) { }
+set ubq_ref(diubq29) { "4s22.pdb" "4s1z.pdb" }
+set ubq_ref(diubq33) { "5af4.pdb" "4xyz.pdb" "5af6.pdb" }
+set ubq_ref(diubq48) { "1aar.pdb" "2pe9.pdb" "3aul.pdb" }
+set ubq_ref(diubq63) { "2jf5-AB.pdb" "3a1q-AB.pdb" "3a1q-DE.pdb" }
+
+# frames for clusters
+
+# 68%
+set cluster_frame(diubq1) { 0 }
+# 53%, 12%
+set cluster_frame(diubq6) { 15 50 }
+# 49%
+set cluster_frame(diubq11) { 15 }
+set cluster_frame(diubq27) {}
+set cluster_frame(diubq29) {}
+set cluster_frame(diubq33) {}
+# 67%
+set cluster_frame(diubq48) { 5 }
+# 77%
+set cluster_frame(diubq63) { 0 }
+
+# 25%
+set cluster_frame(diubq1ll) { 20 }
+#42%, 11%, 10%
+set cluster_frame(diubq6ll) { 10 25 5 }
+# 18%, 10%, 10%
+set cluster_frame(diubq11ll) { 10 16 54 }
+set cluster_frame(diubq27ll) {}
+set cluster_frame(diubq29ll) {}
+set cluster_frame(diubq33ll) {}
+# 40%, 28%
+set cluster_frame(diubq48ll) { 5 0 }
+# 19%, 10%
+set cluster_frame(diubq63ll) { 0 20 }
+
+# representation for comparison structures
+
+proc comparison_reps {m} {
+    mol delrep 0 $m
+
+	set r [molinfo $m get numreps]
+    
+    # molecule bodies
+	mol addrep $m
+	mol modselect $r $m name CA
+	mol modstyle $r $m QuickSurf 1.0 0.5 1.0 1.0
+	mol modcolor $r $m Chain
+	mol modmaterial $r $m Translucent
+    
+    set r [expr $r + 1]
+
+    # hydrophobic patch around Ile44
+	mol addrep $m
+	mol modselect $r $m (resid 8 or resid 44 or resid 68 or resid 70) and name CA
+	mol modstyle $r $m QuickSurf 1.5 0.5 1.0 1.0
+	mol modcolor $r $m ColorID 0
+	mol modmaterial $r $m Diffuse
+    
+    set r [expr $r + 1]
+
+    # hydrophobic patch around Ile36
+	mol addrep $m
+	mol modselect $r $m (resid 36 or resid 71 or resid 73) and name CA
+	mol modstyle $r $m QuickSurf 1.5 0.5 1.0 1.0
+	mol modcolor $r $m ColorID 7
+	mol modmaterial $r $m Diffuse
+
+}
+
+# align (WIP)
+
+proc align_ref {mref f m seltext} {
+    set ref [atomselect $mref $seltext frame $f]
+    set sel [atomselect $m $seltext]
+    set all [atomselect $m all]
+    set n [molinfo $m get numframes]
+
+    for { set i 0 } { $i < $n } { incr i } {
+        $sel frame $i
+        $all frame $i
+        $all move [measure fit $sel $ref]
+    }
+    $ref delete
+    $all delete
+    $sel delete
+    return
+}
+
+# load reference ubq
+
+proc load_ubq_ref {name} {
+    
+    foreach pdbfile $::ubq_ref($name) {
+        set path "$::env(HOME)/repos/cgppd/data/ubiquitin_comparisons/$pdbfile"
+        mol new $path type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+        mol rename top "$name-$pdbfile"
+        mol delrep 0 top
+        comparison_reps top
+    }
+        
+    color Chain A yellow
+    color Chain B orange
+        
+    display resetview
+}
+
+# align these molecules to the given reference molecule
+
+proc align_all {} {
+    set all_mols [molinfo list]
+    set mref [lindex $all_mols 0]
+    set mols [lrange $all_mols 1 end]
+
+    foreach m $mols {
+        align_ref $mref 0 $m "resid 1 to 72 and name CA and chain A and (altloc \"\" or altloc \"A\")" 
+    }
+    
+    display resetview
+}
+
+# Output files
+
+proc load_diubq {linkage} {
+
+    load_ubq "diubq${linkage}"
+    load_ubq "diubq${linkage}ll"
+    load_ubq_ref "diubq${linkage}"
+
+    align_all
+}
+
+proc output_ubq {prefix} {
+    
+    set all_mols [molinfo list]
+    
+    foreach i $all_mols {
+        foreach j $all_mols {
+            mol off $j
+        }
+        
+        mol on $i
+        
+        set name [molinfo $i get name]
+        regexp "^diubq\[0-9\]+" $name dirname
+        set path "$::env(HOME)/repos/cgppd/vmd_exports/$dirname"
+        
+        if { ![string match "*pdb" $name] } {
+            set c 1
+            foreach f $::cluster_frame($name) {
+                animate goto $f
+                render Tachyon $path/$prefix-$name-C$c-$f.dat /usr/local/lib/vmd/tachyon_LINUXAMD64 -aasamples 12 %s -format TARGA -o %s.tga
+                set c [expr $c + 1]
+            }
+        } else {
+            render Tachyon $path/$prefix-$name.dat /usr/local/lib/vmd/tachyon_LINUXAMD64 -aasamples 12 %s -format TARGA -o %s.tga
+        }
+    }
+
+}
+
+# TODO: We need to output 3 views of all molecules aligned *in the same way*. Use one dummy one-frame molecule to load always, and ignore it? Pick one which has a good view of the patches on chain A. Use chain A only?
+# TODO: We should also save this cluster data so that we don't have to keep redoing it.
+
+output_all_ubq {
+# for each linkage
+    set linkages { 1 6 11 27 29 33 48 63 }
+    
+    foreach l $linkages {
+        # TODO load reference
+        load_diubq $l
+        scale by 0.8
+        
+        output_ubq ONE
+        
+        rotate x by 90
+        output_ubq TWO
+        
+        rotate x by 270
+        rotate y by 90
+        output_ubq THREE
+        
+        # TODO delete all molecules
+    }
+}
