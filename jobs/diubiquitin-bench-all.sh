5fe15d94d320449a9cf1c3a33753db2734b5419d
Adrianna Pińska <adrianna.pinska@gmail.com>
2018-02-01 12:08:59 +0200

    more benchmarking jobs

---
+++ jobs/diubiquitin-bench-all.sh
@@ -0,0 +1,10 @@
+#PBS -q GPUQ
+#PBS -l nodes=srvslsgpu002:ppn=4:seriesGPU
+#PBS -N 2ubq_bench_all
+#PBS -V
+
+cd /home/apinska/repos/cgppd
+
+source scripts/hex_setup.sh
+
+./cgppd -f config/diubiquitin_benchmark_all -t 4 -s 4 --gpuoffset 1
