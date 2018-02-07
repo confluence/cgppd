f70aa712baa1bbae9e0d038632a8fcab01a3211f
Adrianna Pińska <adrianna.pinska@gmail.com>
2018-02-07 02:18:48 +0200

    updated benchmarking jobs

--- jobs/diubiquitin-bench-longtail.sh
+++ jobs/diubiquitin-bench-longtail.sh
@@ -1,5 +1,5 @@
 #PBS -q GPUQ
-#PBS -l nodes=srvslsgpu002:ppn=4:seriesGPU
+#PBS -l nodes=srvslsgpu004:ppn=4:seriesGPUk
 #PBS -N 2ubq_bench_longtail
 #PBS -V
 
@@ -7,4 +7,4 @@ cd /home/apinska/repos/cgppd
 
 source scripts/hex_setup.sh
 
-./cgppd -f config/diubiquitin_benchmark_longtail -t 4 -s 4 --gpuoffset 2
+./cgppd -f config/diubiquitin_benchmark_longtail -t 4 -s 4 -g 2 -v 3
