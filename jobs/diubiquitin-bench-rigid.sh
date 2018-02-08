41da518d879f2bafc5242f60d57d2a6214c78ee5
Adrianna Pińska <adrianna.pinska@gmail.com>
2018-02-08 09:00:00 +0200

    changed job parameters again

--- jobs/diubiquitin-bench-rigid.sh
+++ jobs/diubiquitin-bench-rigid.sh
@@ -1,5 +1,5 @@
 #PBS -q GPUQ
-#PBS -l nodes=srvslsgpu004:ppn=4:seriesGPUk
+#PBS -l nodes=srvslsgpu004:ppn=20:seriesGPUk
 #PBS -N 2ubq_bench_rigid
 #PBS -V
 
@@ -7,4 +7,4 @@ cd /home/apinska/repos/cgppd
 
 source scripts/hex_setup.sh
 
-./cgppd -f config/diubiquitin_benchmark_rigid -t 4 -s 4 -g 2 -v 3
+./cgppd -f config/diubiquitin_benchmark_rigid -t 20 -s 20 -g 2 -v 3
