e9b208c443665b1fce222903f7bfdce3a1aa3c89
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2018-02-10 18:38:42 +0200

    jobs

--- jobs/diubiquitin-bench-flex.sh
+++ jobs/diubiquitin-bench-flex.sh
@@ -1,5 +1,5 @@
 #PBS -q GPUQ
-#PBS -l nodes=srvslsgpu004:ppn=20:seriesGPUk
+#PBS -l nodes=srvslsgpu004:ppn=10:seriesGPUk
 #PBS -N 2ubq_bench_flex
 #PBS -V
 
@@ -7,4 +7,4 @@ cd /home/apinska/repos/cgppd
 
 source scripts/hex_setup.sh
 
-./cgppd -f config/diubiquitin_benchmark_flex -t 20 -s 20 -g 2 -v 3
+./cgppd -f config/diubiquitin_benchmark_flex -t 10 -s 10 -v 3 --gpuoffset 1
